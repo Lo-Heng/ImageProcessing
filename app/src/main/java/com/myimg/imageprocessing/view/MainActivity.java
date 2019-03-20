8a966b021704245a7d1727628876fe117b126a70
劳志恒 <623614154@qq.com>
2019-03-20 17:37:12 +0800

    完成80%

--- app/src/main/java/com/myimg/imageprocessing/view/MainActivity.java
+++ app/src/main/java/com/myimg/imageprocessing/view/MainActivity.java
@@ -6,97 +6,136 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.myimg.imageprocessing.R;
 import com.myimg.imageprocessing.view.model.FunctionDrawView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private byte[] rawData;
-    private ImageView imageView;
+    private ImageView ivExampleImage;
     FunctionDrawView mFunctionDrawView;
-    private float[] mPoints = new float[500];
+    private float[] points;
+    List<Float> mFloatArrayList = new ArrayList<Float>();
+    private TextView mTvImgWidth;
+    private TextView mTvImgHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.iv_example_image);
-        //将bitmap转换成一个伪灰度图，再转化成一个byte[]
-        //将byte[]转换为bitmap
-//		Bitmap bitmapOrg = BitmapFactory.decodeByteArray(rawData, 0, rawData.length);
+
+        //绑定
+        ivExampleImage =  findViewById(R.id.iv_example_image);
+        mFunctionDrawView = findViewById(R.id.fv_function);
+        mTvImgHeight = findViewById(R.id.tv_img_height);
+        mTvImgWidth = findViewById(R.id.tv_img_width);
+
+        //读入
         Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), R.drawable.example);
         Bitmap bitmapNew = bitmapOrg.copy(Bitmap.Config.ARGB_8888, true);
-        mFunctionDrawView = findViewById(R.id.fv_function);
-        mFunctionDrawView.getWidth();
-        mFunctionDrawView.getHeight();
-        mFunctionDrawView.invalidate();
+
+        mTvImgHeight.append(" " + bitmapNew.getHeight());
+        mTvImgWidth.append(" " + bitmapNew.getWidth());
+
         if (bitmapNew == null)
             Log.i("TAG", "null");
-        Log.i("TAG", "copy end");
-//        float[] pts = {50, 50, 400, 50,
-//                400, 50, 400, 600,
-//                400, 600, 50, 600,
-//                60, 600, 50, 50};
-//        mFunctionDrawView.setPts(pts);
+
+        startCalculate(200, bitmapNew);
 
 
-        /**
-         * 一种简单的灰度化算法，但是这种方法需要修改每个点的像素，如果一张图片比较大，则这个处理速度就堪忧了
-         * 往往需要创建一个线程来处理
-         */
-        for (int i = 0, k = 0,times = 0; i < bitmapNew.getWidth(); i++) {
-            for (int j = 0; j < bitmapNew.getHeight(); j++) {
-                int col = bitmapNew.getPixel(i, j);//获取点（i，j）处的像素
-                //分别获取ARGB的值
-                int alpha = col & 0xFF000000;
-                int red = (col & 0x00FF0000) >> 16;
-                int green = (col & 0x0000FF00) >> 8;
-                int blue = (col & 0x000000FF);
-
-                //500可设置，就是某一行
-                if (j == 300) {
-                    //用公式X = 0.3×R+0.59×G+0.11×B计算出X代替原来的RGB
-                    int gray = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
-
-                    //100为间隔可调整
-                    if (i % 100 == 0) {
-                        float spacingX = 100;//x坐标的间隔
-                        mPoints[k++] = spacingX * times;//x坐标
-                        mPoints[k++] = gray;//y坐标
-                        times++;
-                        if (k > 3) {
-                            mPoints[k] = mPoints[k-2];
-                            k++;
-                            mPoints[k] = mPoints[k-2];
-                            k++;
-                        }
-                        Log.d("MainActivity", "gray " + " i= " + i + "gray=" + gray + "k= " + k);
-                    }
-                    //新的ARGB
-//                    int newColor = alpha | (gray << 16) | (gray << 8) | gray;
-                    //在点（i,j）处设置新的像素
-//                    bitmapNew.setPixel(i, j, newColor);
-                } else {
-//                    bitmapNew.setPixel(i, j, col);
-                }
-
-                //Log.v("tag",  Integer.toHexString(col));
-            }
+        points = new float[mFloatArrayList.size()];
+        for (int i = 0; i < mFloatArrayList.size(); i++) {
+            points[i] = mFloatArrayList.get(i);
         }
-        mFunctionDrawView.setPts(mPoints);
-        mFunctionDrawView.invalidate();
+
+        mFunctionDrawView.setPts(points);
+
         Log.i("TAG", "pro end");
 //        sendMsg(bitmapNew);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //重新存放数据，以免界面错乱
+        mFunctionDrawView.setPts(points);
+    }
+
+    /**
+     * @创建人 lzh
+     * @方法描述
+     * @参数 row 输入第几行
+     * @创建时间 2019/3/20
+     */
+    private void startCalculate(int row, Bitmap bitmap) {
+
+        for (int i = 0, k = 0, times = 0; i < bitmap.getWidth(); i++) {
+//            for (int j = 0; j < bitmapNew.getHeight(); j++) {
+            int col = bitmap.getPixel(i, row);//获取点的像素
+            //分别获取ARGB的值
+            int alpha = col & 0xFF000000;
+            int red = (col & 0x00FF0000) >> 16;
+            int green = (col & 0x0000FF00) >> 8;
+            int blue = (col & 0x000000FF);
+
+            //500可设置，就是某一行
+            //用公式X = 0.3×R+0.59×G+0.11×B计算出X代替原来的RGB
+            int gray = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
+
+            //100为间隔可调整
+            if (i % 10 == 0) {
+                float spacingX = 10;//x坐标的间隔
+                mFloatArrayList.add(spacingX * times);
+                mFloatArrayList.add((float) gray);
+
+//                mPoints[k++] = spacingX * times;//x坐标
+//                mPoints[k++] = gray;//y坐标
+                times++;
+                if (mFloatArrayList.size() > 3) {
+//                    mPoints[k] = mPoints[k - 2];
+                    mFloatArrayList.add(mFloatArrayList.get(mFloatArrayList.size() - 2));
+                    mFloatArrayList.add(mFloatArrayList.get(mFloatArrayList.size() - 2));
+//                    k++;
+//                    mPoints[k] = mPoints[k - 2];
+//                    k++;
+                }
+                Log.d("MainActivity", "gray " + " i= " + i + "gray=" + gray + "k= " + k);
+            }
+            //新的ARGB
+//                    int newColor = alpha | (gray << 16) | (gray << 8) | gray;
+            //在点（i,j）处设置新的像素
+//                    bitmapNew.setPixel(i, j, newColor);
+
+            //Log.v("tag",  Integer.toHexString(col));
+        }
+//        }
+
+    }
+
     private void sendMsg(Bitmap bitmapNew) {
-        imageView.setImageBitmap(bitmapNew);
+        ivExampleImage.setImageBitmap(bitmapNew);
     }
 }
 
 /**
  * drawableToBitmap
+ * <p>
+ * Bitmap2Bytes
+ * <p>
+ * Bytes2Bimap
+ * <p>
+ * Bitmap2Bytes
+ * <p>
+ * Bytes2Bimap
+ * <p>
+ * Bitmap2Bytes
+ * <p>
+ * Bytes2Bimap
  * <p>
  * Bitmap2Bytes
  * <p>
--- app/src/main/java/com/myimg/imageprocessing/view/model/FunctionDrawView.java
+++ app/src/main/java/com/myimg/imageprocessing/view/model/FunctionDrawView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,6 +15,7 @@ public class FunctionDrawView extends View {
     private float pts[];
     private Paint mPaint;// 创建一个画笔
     private int width, height;
+    private final float AXIS_OFFSET = 7.5f;//坐标轴的偏移量
 
     public FunctionDrawView(Context context) {
         super(context);
@@ -29,7 +31,12 @@ public class FunctionDrawView extends View {
     }
 
     public void setPts(float[] pts) {
-        this.pts = pts;
+        this.pts = pts.clone();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
     }
 
     /**
@@ -45,7 +52,8 @@ public class FunctionDrawView extends View {
         //设置画笔模式
         mPaint.setStyle(Paint.Style.FILL);
         //设置画笔宽度为30px
-        mPaint.setStrokeWidth(5f);
+        mPaint.setStrokeWidth(4f);
+
     }
 
 
@@ -63,17 +71,37 @@ public class FunctionDrawView extends View {
 //        canvas.drawPoint(200, 200, mPaint);
 
         //画坐标轴
-//        draw();
+        drawAxis(canvas);
 
         //绘制直线
         drawLines(canvas);
 
-        //绘制一组点，坐标一一对应
-//        canvas.drawPoints(new float[]{
-//                900, 100,
-//                500, 200,
-//                500, 300
-//        }, mPaint);
+    }
+
+    private void drawAxis(Canvas canvas) {
+        Paint paint = new Paint();
+        //设置画笔颜色
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(5f);
+        //X轴
+        canvas.drawLine(AXIS_OFFSET, height - AXIS_OFFSET, width - 2 * AXIS_OFFSET, height - AXIS_OFFSET, paint);
+        // 绘制箭头
+        Path path = new Path();
+        path.moveTo(width, height - AXIS_OFFSET);
+        path.lineTo(width - AXIS_OFFSET * 2, height - AXIS_OFFSET * 2);
+        path.lineTo(width - AXIS_OFFSET * 2, height);
+        path.close(); // 使这些点构成封闭的多边形
+        canvas.drawPath(path, paint);
+        //y轴
+        canvas.drawLine(AXIS_OFFSET, height - AXIS_OFFSET, AXIS_OFFSET, AXIS_OFFSET * 2, paint);
+        // 绘制箭头
+        Path path1 = new Path();
+        path1.moveTo(AXIS_OFFSET, 0);
+        path1.lineTo(AXIS_OFFSET * 2, AXIS_OFFSET * 2);
+        path1.lineTo(0, AXIS_OFFSET * 2);
+        path1.close(); // 使这些点构成封闭的多边形
+        canvas.drawPath(path1, paint);
 
     }
 
@@ -85,34 +113,35 @@ public class FunctionDrawView extends View {
      * @param canvas
      */
     private void drawLines(Canvas canvas) {
+        float[] ptsTemp = pts.clone();
         float maxX, maxY = 255f;
         maxX = 0;
-        for (int i = 0; i < pts.length; i++) {
-            if(maxX < pts[i]){
-                maxX = pts[i];
+        for (int i = 0; i < ptsTemp.length; i++) {
+            if (maxX < ptsTemp[i]) {
+                maxX = ptsTemp[i];
             }
         }
 
-        for (int i = 0; i < pts.length; i++) {
+        for (int i = 0; i < ptsTemp.length; i++) {
             switch (i % 4) {
                 case 0:
                     //起始点的X坐标
                 case 2:
                     //结束点的X坐标
-                    pts[i] = pts[i] * maxX / width;//按比例转化
-//                    pts[i] += 5;
+                    ptsTemp[i] = ptsTemp[i] * (width - AXIS_OFFSET) / maxX;//按比例转化
+                    ptsTemp[i] += AXIS_OFFSET;
                     break;
                 case 1:
                     //开始点的Y坐标
                 case 3:
                     //结束点的Y坐标
-                    pts[i] = 255 - pts[i];//先反过来
-                    pts[i] = pts[i] * maxY / height;//按比例转化
-//                    pts[i] -= 5;
+                    ptsTemp[i] = 255 - ptsTemp[i];//先反过来
+                    ptsTemp[i] = ptsTemp[i] * (height - AXIS_OFFSET) / maxY;//按比例转化
+                    ptsTemp[i] -= AXIS_OFFSET;
                     break;
             }
 
         }
-        canvas.drawLines(pts, mPaint);
+        canvas.drawLines(ptsTemp, mPaint);
     }
 }
